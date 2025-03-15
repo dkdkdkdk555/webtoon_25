3ad25ae75c9116b2256dc69f05904d2d1f2b1dbd
dkdkdkdk555 <dkdkdkdk555@naver.com>
2025-03-15 12:01:09 +0900

    리스트뷰 스크롤방향 정함

--- lib/view/webtoon_list.dart
+++ lib/view/webtoon_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:webtoon_25/service/webtoon_model.dart';
 import 'package:webtoon_25/view_model/webtoon_list_view_model.dart';
 
@@ -31,39 +30,33 @@ class _WebtoonListState extends State<WebtoonList> {
          if (webtoonListViewModel.list == null || webtoonListViewModel.list!.isEmpty) {
           return const Center(child: CircularProgressIndicator()); // ✅ 로딩 UI 추가
         }
-        return Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: webtoonListViewModel.list!.length,
-                itemBuilder: (context, idx){
-                  var webtoon = webtoonListViewModel.list![idx];
-                  return Column(
-                    children: [
-                      Image.network(
-                          webtoon.thumb,
-                          headers: const { 
-                            // 밑에는 안드로이드 나 IOS 를 에뮬레이터를 사용할 경우 CORS 문제를 해결하기 위해 넣어야 하는 헤더 내용이다.
-                            // 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
-                            // 'Referer': 'https://comic.naver.com',
-                      
-                            // chrome 에서 할때는 
-                            // flutter run -d chrome --web-renderer html
-                            // 명령어로 실행하여 해결함. HTML 랜더링 엔진을 사용하도록 설정하는 것
-                          },
+        return Expanded(
+          child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: webtoonListViewModel.list!.length,
+                  itemBuilder: (context, idx){
+                    var webtoon = webtoonListViewModel.list![idx];
+                    return Column(
+                      children: [
+                        Image.network(
+                            webtoon.thumb,
+                            headers: const { 
+                              // 밑에는 안드로이드 나 IOS 를 에뮬레이터를 사용할 경우 CORS 문제를 해결하기 위해 넣어야 하는 헤더 내용이다.
+                              // 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+                              // 'Referer': 'https://comic.naver.com',
+                        
+                              // chrome 에서 할때는 
+                              // flutter run -d chrome --web-renderer html
+                              // 명령어로 실행하여 해결함. HTML 랜더링 엔진을 사용하도록 설정하는 것
+                            },
+                          ),
+                        Text(
+                          webtoon.title
                         ),
-                      Text(
-                        webtoon.title
-                      ),
-                    ],
-                  );
+                      ],
+                    );
                 }
-              ),
-            )
-          ],
+          ),
         );
       }
     );
